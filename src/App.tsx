1671713ac40b4b01847af7d42c6ae24cab46e6d5
horizonnns <shadowcrewhack@gmail.com>
2026-02-28 21:19:18 +0500

    #11 Added emptiness)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import DashboardLayout from "./layouts/DashboardLayout";
-import DashboardPage from "./pages/DashboardPage";
-import WelcomePage from "./pages/WelcomePage";
-import { useSelector } from "react-redux";
-import type { RootState } from "./store/store";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import {
+	Navigate,
+	Route,
+	BrowserRouter as Router,
+	Routes
+} from 'react-router-dom'
+import DashboardLayout from './layouts/DashboardLayout'
+import DashboardPage from './pages/DashboardPage'
+import WelcomePage from './pages/WelcomePage'
+import type { RootState } from './store/store'
 
 const AppContent: React.FC = () => {
-  const { selectedBotId } = useSelector((state: RootState) => state.bots);
+	const { selectedBotId } = useSelector((state: RootState) => state.bots)
 
-  return (
-    <Routes>
-      <Route element={<DashboardLayout />}>
-        <Route path="/" element={selectedBotId ? <DashboardPage /> : <WelcomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
-  );
-};
+	return (
+		<Routes>
+			<Route element={<DashboardLayout />}>
+				<Route
+					path='/'
+					element={selectedBotId ? <DashboardPage /> : <WelcomePage />}
+				/>
+				<Route
+					path='*'
+					element={
+						<Navigate
+							to='/'
+							replace
+						/>
+					}
+				/>
+			</Route>
+		</Routes>
+	)
+}
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<AppContent />
+		</Router>
+	)
+}
 
-export default App;
+export default App
